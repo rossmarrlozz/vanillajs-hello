00ae9449cf5c138a06791f6067fed5748f6e0c21
rossmarrlozz <168719962+rossmarrlozz@users.noreply.github.com>
2024-06-20 09:44:18 +0000

    Pending changes exported from your codespace

--- src/app.js
+++ src/app.js
@@ -6,6 +6,30 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["My homework", "The keys", "the car"];
+  let when = [
+    "before the class",
+    "rigth on time",
+    "when i finished",
+    "during my lunch",
+    "while i was praying"
+  ];
+  const getRandomElement = listElemnts => {
+    let element = "";
+    element = listElemnts[Math.floor(Math.random() * listElemnts.length)];
+    return element;
+  };
+  let excuseGen = document.querySelector("#excuse");
+  console.log(excuseGen);
+  excuseGen.innerHTML =
+    getRandomElement(who) +
+    " " +
+    getRandomElement(action) +
+    " " +
+    getRandomElement(what) +
+    " " +
+    getRandomElement(when);
   console.log("Hello Rigo from the console!");
 };
